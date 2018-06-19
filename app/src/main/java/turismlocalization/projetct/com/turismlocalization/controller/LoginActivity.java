3dc1298439e535bef0a8e42b75fe1b04c4fd2665
Humbert0Mendes <hmpn87@gmail.com>
2018-06-19 13:56:09 -0300

    Inicializando repositório

---
+++ app/src/main/java/turismlocalization/projetct/com/turismlocalization/controller/LoginActivity.java
@@ -0,0 +1,16 @@
+package turismlocalization.projetct.com.turismlocalization.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import turismlocalization.projetct.com.turismlocalization.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
