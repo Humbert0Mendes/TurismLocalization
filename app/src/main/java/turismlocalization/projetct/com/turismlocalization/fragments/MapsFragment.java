5aad5fbcab968d668341bf5fd7d1803f9ca0a9bf
Humbert0Mendes <hmpn87@gmail.com>
2018-07-19 01:00:32 -0300

    Localização implementada com sucesso!

--- app/src/main/java/turismlocalization/projetct/com/turismlocalization/fragments/MapsFragment.java
+++ app/src/main/java/turismlocalization/projetct/com/turismlocalization/fragments/MapsFragment.java
@@ -1,6 +1,10 @@
 package turismlocalization.projetct.com.turismlocalization.fragments;
 
+import android.content.Context;
+import android.location.Criteria;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -14,6 +18,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsFragment extends SupportMapFragment implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
 
     private GoogleMap mMap;
+    private LocationManager locationManager;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -33,18 +38,31 @@ public class MapsFragment extends SupportMapFragment implements OnMapReadyCallba
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        mMap.setOnMapClickListener(this);
-        mMap.getUiSettings().setZoomControlsEnabled(true);
 
-        // Add a marker in Sydney and move the  camera
-        LatLng sydney = new LatLng(-33.87365, 151.20689);
+        try {
 
-        MarkerOptions mark = new MarkerOptions();
-        mark.position(sydney);
-        mark.title("Marker in Sidney");
-        mMap.addMarker(mark);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+            Criteria criteria = new Criteria();
+            String provider = locationManager.getBestProvider(criteria, true);
+
+            Toast.makeText(getActivity(), "Provider: "+provider, Toast.LENGTH_SHORT).show();
+
+            mMap = googleMap;
+            mMap.setOnMapClickListener(this);
+            mMap.getUiSettings().setZoomControlsEnabled(true);
+            mMap.setMyLocationEnabled(true);
+
+        }catch (SecurityException err){
+            Log.i("Error Map : ", "Erro ao iniciar GoogleMaps"+err);
+        }
+
+            // Add a marker in Sydney and move the  camera
+            LatLng sydney = new LatLng(-33.87365, 151.20689);
+            MarkerOptions mark = new MarkerOptions();
+            mark.position(sydney);
+            mark.title("Marker in Sidney");
+            mMap.addMarker(mark);
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
     @Override
